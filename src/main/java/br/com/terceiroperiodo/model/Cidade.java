20f368acb7a489c2d2befbdb4169953e3b2678c8
willianedu <willianeduardo9877@hotmail.com>
2023-05-09 19:25:48 -0300

    create atribute in Cidade

--- src/main/java/br/com/terceiroperiodo/model/Cidade.java
+++ src/main/java/br/com/terceiroperiodo/model/Cidade.java
@@ -1,8 +1,19 @@
 package br.com.terceiroperiodo.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Cidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     String nome;
+    @ManyToOne
+    @JoinColumn(name = "id_estado")
     Estado estado;
 
 }
